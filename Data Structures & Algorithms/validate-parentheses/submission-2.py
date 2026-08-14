7dbfd905a02036525c85050e42ef1be3e2e16379
niti-csds <jainniti347@gmail.com>
2026-08-14 22:06:16 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.py
@@ -0,0 +1,25 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        dict = {'{':'}','[':']','(':')'}
+        mid = len(s)//2
+        res = []
+        if len(s)%2 != 0:
+            return False
+        for i in range(len(s)):
+            if s[i] == '{' or s[i] == '[' or s[i] == '(':
+                res.append(s[i])
+            elif not res:
+                return False
+            else:
+                if res:
+                    p = res.pop()
+                    if dict[p] != s[i]:
+                        return False
+                    
+                    
+        if not res:
+            return True
+        else:
+            return False
+ 
+        
