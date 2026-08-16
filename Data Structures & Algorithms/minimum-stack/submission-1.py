660498409f7576be3831739468078b90a420745f
niti-csds <jainniti347@gmail.com>
2026-08-16 20:21:26 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.py
@@ -0,0 +1,23 @@
+class MinStack:
+
+    def __init__(self):
+        self.arr = []
+
+
+    def push(self, val: int) -> None:
+        self.arr.append(val)
+
+    def pop(self) -> None:
+        self.arr = self.arr[:-1]
+
+    def top(self) -> int:
+        return self.arr[-1] if self.arr else None       
+
+    def getMin(self) -> int:
+        mini = self.arr[0]
+        for n in self.arr:
+            if n < mini:
+                mini = n
+        return mini
+
+        
